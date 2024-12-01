e4ca951277b30f1c7cbfea77af80652d2ef74571
Raunak Chowdhury <raunakchowdhury@Raunaks-MacBook-Pro-2.local>
2024-11-30 19:17:30 -0500

    initial template creation

---
+++ src/client/java/name/raremobesp/RareMobESPClient.java
@@ -0,0 +1,10 @@
+package name.raremobesp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RareMobESPClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
